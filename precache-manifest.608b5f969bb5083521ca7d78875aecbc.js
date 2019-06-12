dfab1b28fb70f89e9695e883f071cba72644d669
Luis León <ljle.06@gmail.com>
2019-06-12 15:57:49 -0400

    Updates

---
+++ precache-manifest.608b5f969bb5083521ca7d78875aecbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7f30c244684b8ba2f37bacab16ed66",
+    "url": "/hooks-todo-example/index.html"
+  },
+  {
+    "revision": "68ef16f0639a74fff430",
+    "url": "/hooks-todo-example/static/css/main.e41a6a6d.chunk.css"
+  },
+  {
+    "revision": "bae1033d2d9b79e2459d",
+    "url": "/hooks-todo-example/static/js/2.50a74ccf.chunk.js"
+  },
+  {
+    "revision": "68ef16f0639a74fff430",
+    "url": "/hooks-todo-example/static/js/main.69820b94.chunk.js"
+  },
+  {
+    "revision": "5810e113b02ec315ee37",
+    "url": "/hooks-todo-example/static/js/runtime~main.6e116730.js"
+  }
+]);
